950f25087728f761d399a8942283afe237e3a3b4
Anastasiya Saakyan <vv999vv@bk.ru>
2022-08-27 14:39:41 +0300

    Initial commit+

---
+++ src/MagicBox.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class MagicBox<T> {
+    protected T[] items;
+
+    public MagicBox(int maxItems) {
+
+        this.items = (T[]) new Object[maxItems];
+    }
+
+    int filled = 0;
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                filled += 0;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        Random random = new Random();
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Коробка не заполнена! Осталось заполнить: " + (items.length - filled) + " ячеек.");
+            }
+        }
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                int randomInt = random.nextInt(items.length);
+                System.out.println("Коробка заполнена! Случайный элемент из коробки: " + items[randomInt]);
+                System.out.println();
+                return items[randomInt];
+            }
+        }
+        return null;
+    }
+}
